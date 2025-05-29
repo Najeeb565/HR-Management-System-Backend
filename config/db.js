d24c74086a637b5c4f0ddabd51cdda2e54b9da72
Toseeq <toseq.20aug24webbpt@gmail.com>
2025-05-29 22:43:24 +0500

    adminlogin backend

--- config/db.js
+++ config/db.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb+srv://najeeb4aug24webbpt:NajeeB%40565982@cluster0.qdbit.mongodb.net/Company', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log('✅ MongoDB Connected');
   } catch (err) {
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // server/index.js
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-
 const connectDB = require('./config/db'); // MongoDB connection
 const authRoutes = require('./Routes/authroutes');
 const { registerCompany } = require('./Controller/auth'); // Register company route
-
+const superadmin = require('./Routes/superadminroutes'); // Superadmin routes
 const app = express();
 const PORT = 5000;
 
+
+dotenv.config();
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
