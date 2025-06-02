a14db8e095f56fb9c39aeb575e063d47a70935b1
Najeeb565 <najeebbhatti565@gmail.com>
2025-06-02 18:57:22 +0500

    updated

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://najeeb4aug24webbpt:NajeeB%40565982@cluster0.qdbit.mongodb.net/compnay'),
+    await mongoose.connect('mongodb+srv://najeeb4aug24webbpt:NajeeB%40565982@cluster0.qdbit.mongodb.net/company')
     console.log('✅ MongoDB Connected');
   } catch (err) {
     console.error('❌ MongoDB connection failed:', err.message);
